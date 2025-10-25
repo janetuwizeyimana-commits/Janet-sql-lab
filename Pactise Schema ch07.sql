4a6a77b9418358e317037de6de66071fd972086e
janetuwizeyimana-commits <janetuwizeyimana@gmail.com>
2025-10-25 16:07:57 -0700

    Add files via upload
    chapter07_schema.sql

---
+++ Pactise Schema ch07.sql
@@ -0,0 +1,254 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- ----------------------------------------------------- 
+-- Schema chapter07
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `chapter07` ;
+
+-- -----------------------------------------------------
+-- Schema chapter07
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `chapter07`;
+USE `chapter07` ;
+
+-- -----------------------------------------------------
+-- Table `chapter07`.`customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `chapter07`.`customer` ;
+
+CREATE TABLE `customer` (
+  `CUS_CODE` INT NOT NULL,
+  `CUS_LNAME` VARCHAR(15) ,
+  `CUS_FNAME` VARCHAR(15) ,
+  `CUS_INITIAL` VARCHAR(1) ,
+  `CUS_AREACODE` VARCHAR(3) ,
+  `CUS_PHONE` VARCHAR(8) ,
+  `CUS_BALANCE` FLOAT ,
+  PRIMARY KEY (`CUS_CODE`));
+
+
+
+-- -----------------------------------------------------
+-- Table `chapter07`.`invoice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `chapter07`.`invoice`;
+
+CREATE TABLE `invoice` (
+  `INV_NUMBER` INT NOT NULL,
+  `CUS_Code` INT NOT NULL,
+  `INV_DATE` DATETIME NULL,
+  PRIMARY KEY (`INV_NUMBER`),
+  FOREIGN KEY (`CUS_Code`) REFERENCES `customer` (`CUS_CODE`)
+  );
+
+
+-- -----------------------------------------------------
+-- Table `chapter07`.`vendor`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `chapter07`.`vendor` ;
+
+CREATE TABLE `vendor` (
+  `V_CODE` INT NOT NULL,
+  `V_NAME` VARCHAR(15) NULL DEFAULT NULL,
+  `V_CONTACT` VARCHAR(50) NULL DEFAULT NULL,
+  `V_AREACODE` VARCHAR(3) NULL DEFAULT NULL,
+  `V_PHONE` VARCHAR(8) NULL DEFAULT NULL,
+  `V_STATE` VARCHAR(2) NULL DEFAULT NULL,
+  `V_ORDER` VARCHAR(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`V_CODE`));
+
+-- -----------------------------------------------------
+-- Table `chapter07`.`product`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `product` ;
+
+CREATE TABLE `product` (
+  `P_CODE` VARCHAR(10) NOT NULL,
+  `P_DESCRIPT` VARCHAR(35) NULL DEFAULT NULL,
+  `P_INDATE` DATETIME NULL DEFAULT NULL,
+  `P_QOH` SMALLINT NULL DEFAULT NULL,
+  `P_MIN` SMALLINT NULL DEFAULT NULL,
+  `P_PRICE` FLOAT NULL DEFAULT NULL,
+  `P_DISCOUNT` DOUBLE NULL DEFAULT NULL,
+  `V_CODE` INT NULL,
+  PRIMARY KEY (`P_CODE`),
+  FOREIGN KEY (`V_CODE`) REFERENCES `vendor` (`V_CODE`)
+  );
+
+
+
+-- -----------------------------------------------------
+-- Table `chapter07`.`line`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `line` ;
+
+CREATE TABLE `line` (
+  `INV_NUMBER` INT NOT NULL,
+  `LINE_NUMBER` SMALLINT NOT NULL,
+  `P_CODE` VARCHAR(10) NULL,
+  `LINE_UNITS` DOUBLE NULL DEFAULT NULL,
+  `LINE_PRICE` FLOAT NULL DEFAULT NULL,
+  PRIMARY KEY (`INV_NUMBER`, `LINE_NUMBER`),
+  CONSTRAINT `fk_line_product1`
+    FOREIGN KEY (`P_CODE`)
+    REFERENCES `chapter07`.`product` (`P_CODE`),
+  CONSTRAINT `fk_line_invoice1`
+    FOREIGN KEY (`INV_NUMBER`)
+    REFERENCES `chapter07`.`invoice` (`INV_NUMBER`)
+);
+
+
+
+INSERT INTO CUSTOMER VALUES('10010','Ramas','Alfred','A','615','844-2573','0');
+INSERT INTO CUSTOMER VALUES('10011','Dunne','Leona','K','713','894-1238','0');
+INSERT INTO CUSTOMER VALUES('10012','Smith','Kathy','W','615','894-2285','345.859985351562');
+INSERT INTO CUSTOMER VALUES('10013','Olowski','Paul','F','615','894-2180','536.75');
+INSERT INTO CUSTOMER VALUES('10014','Orlando','Myron','','615','222-1672','0');
+INSERT INTO CUSTOMER VALUES('10015','O''Brian','Amy','B','713','442-3381','0');
+INSERT INTO CUSTOMER VALUES('10016','Brown','James','G','615','297-1228','221.190002441406');
+INSERT INTO CUSTOMER VALUES('10017','Williams','George','','615','290-2556','768.929992675781');
+INSERT INTO CUSTOMER VALUES('10018','Farriss','Anne','G','713','382-7185','216.550003051758');
+INSERT INTO CUSTOMER VALUES('10019','Smith','Olette','K','615','297-3809','0');
+
+
+INSERT INTO INVOICE VALUES('1001','10014','2012-01-16');
+INSERT INTO INVOICE VALUES('1002','10011','2012-01-16');
+INSERT INTO INVOICE VALUES('1003','10012','2012-01-16');
+INSERT INTO INVOICE VALUES('1004','10011','2012-01-17');
+INSERT INTO INVOICE VALUES('1005','10018','2012-01-17');
+INSERT INTO INVOICE VALUES('1006','10014','2012-01-17');
+INSERT INTO INVOICE VALUES('1007','10015','2012-01-17');
+INSERT INTO INVOICE VALUES('1008','10011','2012-01-17');
+
+
+
+INSERT INTO VENDOR VALUES('21225','Bryson, Inc.','Smithson','615','223-3234','TN','Y');
+INSERT INTO VENDOR VALUES('21226','SuperLoo, Inc.','Flushing','904','215-8995','FL','N');
+INSERT INTO VENDOR VALUES('21231','D&E Supply','Singh','615','228-3245','TN','Y');
+INSERT INTO VENDOR VALUES('21344','Gomez Bros.','Ortega','615','889-2546','KY','N');
+INSERT INTO VENDOR VALUES('22567','Dome Supply','Smith','901','678-1419','GA','N');
+INSERT INTO VENDOR VALUES('23119','Randsets Ltd.','Anderson','901','678-3998','GA','Y');
+INSERT INTO VENDOR VALUES('24004','Brackman Bros.','Browning','615','228-1410','TN','N');
+INSERT INTO VENDOR VALUES('24288','ORDVA, Inc.','Hakford','615','898-1234','TN','Y');
+INSERT INTO VENDOR VALUES('25443','B&K, Inc.','Smith','904','227-0093','FL','N');
+INSERT INTO VENDOR VALUES('25501','Damal Supplies','Smythe','615','890-3529','TN','N');
+INSERT INTO VENDOR VALUES('25595','Rubicon Systems','Orton','904','456-0092','FL','Y');
+
+
+
+INSERT INTO PRODUCT VALUES('11QER/31','Power painter, 15 psi. 3-nozzle','2011-03-11','8','5','109.99','0','25595');
+INSERT INTO PRODUCT VALUES('13-Q2/P2','7.25-in. pwr. saw blade','2011-12-13','32','15', '14.99','0.05','21344');
+INSERT INTO PRODUCT VALUES('14-Q1/L3','9.00-in. pwr. saw blade','2011-12-11','18','12','17.49','0','21344');
+INSERT INTO PRODUCT VALUES('1546-QQ2','Hrd. cloth, 1/4-in., 2x50','2012-01-15','15','8','39.95','0','23119');
+INSERT INTO PRODUCT VALUES('1558-QW1','Hrd. cloth, 1/2-in., 3x50','2012-01-15','23','5','43.99','0','23119');
+INSERT INTO PRODUCT VALUES('2232/QTY','B\&D jigsaw, 12-in. blade','2011-12-30','8','5','109.92','0.05','24288');
+INSERT INTO PRODUCT VALUES('2232/QWE','B\&D jigsaw, 8-in. blade','2011-12-24','6','5','99.87','0.05','24288');
+INSERT INTO PRODUCT VALUES('2238/QPD','B\&D cordless drill, 1/2-in.','2012-01-20','12','5','38.95','0.05','25595');
+INSERT INTO PRODUCT VALUES('23109-HB','Claw hammer','2012-01-20','23','10','9.95','0.1','21225');
+INSERT INTO PRODUCT VALUES('23114-AA','Sledge hammer, 12 lb.','2012-01-20','8','5','14.40','0.05',NULL);
+INSERT INTO PRODUCT VALUES('54778-2T','Rat-tail file, 1/8-in. fine','2011-12-15','43','20','4.99','0','21344');
+INSERT INTO PRODUCT VALUES('89-WRE-Q','Hicut chain saw, 16 in.','2012-02-07','11','5','256.99','0.05','24288');
+INSERT INTO PRODUCT VALUES('PVC23DRT','PVC pipe, 3.5-in., 8-ft','2012-02-20','188','75','5.87','0',NULL);
+INSERT INTO PRODUCT VALUES('SM-18277','1.25-in. metal screw, 25','2012-03-01','172','75','6.99','0','21225');
+INSERT INTO PRODUCT VALUES('SW-23116','2.5-in. wd. screw, 50','2012-02-24','237','100','8.45','0','21231');
+INSERT INTO PRODUCT VALUES('WR3/TT3','Steel matting, 4''x8''x1/6", .5" mesh','2012-01-17','18','5','119.95','0.1','25595');
+
+
+
+INSERT INTO LINE VALUES('1001','1','13-Q2/P2','1','14.99');
+INSERT INTO LINE VALUES('1001','2','23109-HB','1','9.95');
+INSERT INTO LINE VALUES('1002','1','54778-2T','2','4.99');
+INSERT INTO LINE VALUES('1003','1','2238/QPD','1','38.95');
+INSERT INTO LINE VALUES('1003','2','1546-QQ2','1','39.95');
+INSERT INTO LINE VALUES('1003','3','13-Q2/P2','5','14.99');
+INSERT INTO LINE VALUES('1004','1','54778-2T','3','4.99');
+INSERT INTO LINE VALUES('1004','2','23109-HB','2','9.95');
+INSERT INTO LINE VALUES('1005','1','PVC23DRT','12','5.87');
+INSERT INTO LINE VALUES('1006','1','SM-18277','3','6.99');
+INSERT INTO LINE VALUES('1006','2','2232/QTY','1','109.92');
+INSERT INTO LINE VALUES('1006','3','23109-HB','1','9.95');
+INSERT INTO LINE VALUES('1006','4','89-WRE-Q','1','256.99');
+INSERT INTO LINE VALUES('1007','1','13-Q2/P2','2','14.99');
+INSERT INTO LINE VALUES('1007','2','54778-2T','1','4.99');
+INSERT INTO LINE VALUES('1008','1','PVC23DRT','5','5.87');
+INSERT INTO LINE VALUES('1008','2','WR3/TT3','3','119.95');
+INSERT INTO LINE VALUES('1008','3','23109-HB','1','9.95');
+
+USE CHAPTER07;
+-- Question1
+SELECT P_CODE, P_DESCRIPT, P_QOH, P_PRICE
+FROM PRODUCT;
+
+-- QUESTION2
+SELECT P_CODE, P_DESCRIPT, P_PRICE, P_QOH, P_QOH*P_PRICE
+FROM product;
+
+-- QUESTION3
+SELECT P_CODE, P_DESCRIPT, P_PRICE, P_QOH, (P_PRICE*P_QOH) AS Totalvalue
+FROM product;
+
+-- QUESTION4 
+SELECT P_INDATE, DATE_ADD(P_INDATE, INTERVAL 90 DAY) AS EXPIRATION
+FROM product;
+
+-- QUESTION5
+SELECT DISTINCT V_CODE
+FROM product;
+
+-- QUESTION6
+SELECT * 
+FROM PRODUCT
+ORDER BY P_PRICE ASC;
+
+-- QUESTION7
+SELECT* 
+FROM product
+ORDER BY P_PRICE DESC;
+
+-- QUESTION8
+select CUS_LNAME, CUS_FNAME, CUS_PHONE,CUS_INITIAL
+from customer
+order by CUS_LNAME, CUS_FNAME, CUS_INITIAL; 
+
+-- QUESTION9
+select*
+From product
+order by P_QOH*P_PRICE DESC;
+
+-- Question10
+SELECT*
+FROM product
+ORDER BY V_CODE DESC;
+
+-- QUESTION11
+SELECT *
+FROM product
+ORDER BY V_CODE is null DESC;
+
+-- QUESTIONS12
+SELECT P_DESCRIPT, P_QOH, P_PRICE, V_CODE
+FROM product
+WHERE V_CODE =21344; 
+
+-- QUESTION13
+SELECT P_DESCRIPT, P_QOH, P_PRICE, V_CODE
+FROM product
+WHERE V_CODE <>21344; 
+
+-- QUESTION14
+ SELECT P_DESCRIPT, P_QOH, P_PRICE, V_CODE
+FROM product
+WHERE P_PRICE <=10;
+
+-- QUESTION15
+select*
+from product
+where P_CODE > "1558QW1";
+ 
